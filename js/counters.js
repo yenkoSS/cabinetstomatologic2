f0b3d9b13040893b65694d0716e684240a3b8542
Stefan Petru-Cornel <petrucornel.stefan@gmail.com>
2025-08-27 02:24:48 +0300

    +

--- js/counters.js
+++ js/counters.js
@@ -1,16 +1,25 @@
 let upToClients = 330;
+let upToYears = 5;
 
 function updateClientsValue() {
   let count = document.querySelector(".text-counter-clients");
   const increased = ++upToClients;
-  count.innerHTML = increased.toLocaleString() + " + <span>clienți fericiți</span>";
+  count.innerHTML = increased.toLocaleString() + " + <span class='fs-20 fw-700 d-block mtop-20'>clienți fericiți</span>";
   if (upToClients === 400) {
     clearInterval(countClients);
   }
 }
 
-let countExperience;
-let countCars;
+function updateYearsValue() {
+  let count = document.querySelector(".text-counter-years");
+  const increased = ++upToYears;
+  count.innerHTML = increased.toLocaleString() + " + <span class='fs-20 fw-700 d-block mtop-20'>ani de experiență</span>";
+  if (upToYears === 10) {
+    clearInterval(countYears);
+  }
+}
+
+let countYears;
 let countClients;
 
 const observer = new IntersectionObserver(
@@ -27,4 +36,19 @@ const observer = new IntersectionObserver(
   { threshold: 0.5 }
 );
 
-document.querySelectorAll(".text-counter-xp, .text-counter-cars, .text-counter-clients").forEach((el) => observer.observe(el));
+const observerYears = new IntersectionObserver(
+  (entries, obs) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        if (entry.target.classList.contains("text-counter-years") && !countYears) {
+          countYears = setInterval(updateYearsValue, 500);
+        }
+        obs.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
+
+document.querySelectorAll(".text-counter-clients").forEach((el) => observer.observe(el));
+document.querySelectorAll(".text-counter-years").forEach((el) => observerYears.observe(el));
